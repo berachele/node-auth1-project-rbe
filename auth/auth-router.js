889901b2c6a77a476dffa626c71f4208cb449411
Rachele Edwards <dot.com1472@gmail.com>
2020-05-18 14:35:33 -0600

    added sessions and implements in login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -37,7 +37,35 @@ router.post('/signup', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    const {username, password} = req.body
 
+    if(isValid(req.body)){
+        User.findBy({username: username})
+        .then(([user]) => {
+            if(user && bcrypt.compareSync(password, user.password)){
+                req.session.loggedIn = true
+                req.session.user = user
+                
+                res.status(200).json({
+                    message: `Welcome, ${user.username}`
+                })
+            }else{
+                res.status(401).json({
+                    message: "Invalid credientials"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: err.message
+            })
+        })
+    }else{
+        res.status(400).json({
+            message: "Please provide username and password. Password must be alphanumeric."
+        })
+    }
 })
 
 router.get('/logout', (req, res) => {
--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const userRouter = require('./users/user-router')
 const authRouter = require('./auth/auth-router')
 
 const server = express()
+const session = require('express-session')
 
+const sessionConfig = {
+    name: "nomad",
+    secret: process.env.COOKIE_SECRET || "SECRET-TUNNEL!",
+    cookie: {
+        maxAge: 1000 * 60 * 30, //30 minutes
+        secure: process.env.SECURE_COOKIE || false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: process.send.USER_ALLOW_COOKIES || true
+}
 
 server.use(express.json())
+server.use(session(sessionConfig))
 
 server.get('/', (req, res) => {
     res.status(200).send("SUCCESS!")
