842eb572bc212600dab2c453c88d2e260b3ca35e
Rachele Edwards <dot.com1472@gmail.com>
2020-05-18 13:55:07 -0600

    setup user-router

--- users/user-router.js
+++ users/user-router.js
@@ -6,7 +6,14 @@ const User = require('./user-model')
 
 //bringing in from /api/users
 router.get("/'", (req, res) => {
-    
+    User.find()
+    .then()
+    .catch(err => {
+        console.log({err})
+        res.status(500).json({
+            message: err.message
+        })
+    })
 })
 
 module.exports = router
