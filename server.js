158b363f92f8d6ecc4038cced74efbb89934b989
Rachele Edwards <dot.com1472@gmail.com>
2020-05-18 14:06:17 -0600

    get testing works

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = ('express')
+const express = require('express')
 
 const userRouter = require('./users/user-router')
 
 const server = express()
 
+
 server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).send("SUCCESS!")
+})
 
 server.use('/api/users', userRouter)
 
--- users/user-router.js
+++ users/user-router.js
@@ -5,9 +5,11 @@ const router = express.Router()
 const User = require('./user-model')
 
 //bringing in from /api/users
-router.get("/'", (req, res) => {
+router.get('/', (req, res) => {
     User.find()
-    .then()
+    .then(list => {
+        res.status(200).json(list)
+    })
     .catch(err => {
         console.log({err})
         res.status(500).json({
